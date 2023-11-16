d44ad93191861a11425557c5f7e4421e1a0758fe
violagul <viola.gullace@sns.it>
2023-11-16 11:16:16 +0100

    Update Classifier_neg_cls_compl.py

--- 2nd_exp/Classifier_neg_cls_compl.py
+++ 2nd_exp/Classifier_neg_cls_compl.py
@@ -258,18 +258,18 @@ model_mask = AutoModelForMaskedLM.from_pretrained('dbmdz/bert-base-italian-cased
 
 # load names, professions and verbs for the templates
 path = r"../Inputs"
-fName_file_path = f"{path}\100_names_f.txt"
-mName_file_path = f"{path}\100_names_m.txt"
-fProf_file_path = f"{path}\100_mestieri_f.txt"
-mProf_file_path = f"{path}\100_mestieri_m.txt"
-hypo_file_path = f"{path}\frasi_it.txt"
+fName_file_path = f"{path}/100_names_f.txt"
+mName_file_path = f"{path}/100_names_m.txt"
+fProf_file_path = f"{path}/100_mestieri_f.txt"
+mProf_file_path = f"{path}/100_mestieri_m.txt"
+hypo_file_path = f"{path}/frasi_it.txt"
 
 fName_file = open(fName_file_path, "r")
 mName_file = open(mName_file_path, "r")
 fProf_file = open(fProf_file_path, "r")
 mProf_file = open(mProf_file_path, "r")
 
-list_verbs = load(f"{path}\base_verbs.joblib")
+list_verbs = load(f"{path}/base_verbs.joblib")
 
 
 
