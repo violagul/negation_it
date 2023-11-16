23fe7583afc1722d854f9c7daf789a83e26a1e14
violagul <viola.gullace@sns.it>
2023-11-16 12:06:16 +0100

    Create prova.py

---
+++ 2nd_exp/prova.py
@@ -0,0 +1,46 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = "0"
+
+import torch
+from joblib import load, dump
+from transformers import AutoTokenizer, AutoModel, AutoModelForMaskedLM
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import confusion_matrix
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.utils import shuffle as skshuffle
+import re
+from random import shuffle, seed
+
+#from sklearn.model_selection import train_test_split
+
+from tools.build_array import build_array, build_hypo, build_masked_context
+from tools.chech_conjug import check_conjugation
+
+#from tools.chech_conjug import check_conjugation
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.devide("cpu")
+
+from tools.build_array import build_array, build_hypo, build_masked_context
+from tools.chech_conjug import check_conjugation
+
+
+model = AutoModel.from_pretrained('dbmdz/bert-base-italian-cased').to(device)
+tokenizer = AutoTokenizer.from_pretrained('dbmdz/bert-base-italian-cased')
+
+
+current batch = "Sono una frase da analizzare"
+
+with torch.no_grad():
+        # encode sentences
+        encoded_sentence = tokenizer.batch_encode_plus(current_batch,padding=True,  return_tensors="pt").to(device)
+        # get the mask-token index in the sentence 
+        mask_tokens_index = torch.where(encoded_sentence['input_ids'] == tokenizer.mask_token_id) 
+        #print(mask_tokens_index)
+        # get logits vectors
+        tokens_logits = model(**encoded_sentence) 
+        #print(tokens_logits)
+        #print(tokens_logits['logits'].shape)
+
+        # get the mask-token logit
+        mask_tokens_logits = tokens_logits['logits'][ mask_tokens_index]
