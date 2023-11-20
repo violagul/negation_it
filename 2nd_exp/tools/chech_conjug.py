0c964e512417dc044c68b5b8d4a891397a442460
violagul <viola.gullace@sns.it>
2023-11-20 15:21:01 +0100

    Create chech_conjug.py

---
+++ 2nd_exp/tools/chech_conjug.py
@@ -0,0 +1,54 @@
+# controlla se un vb flesso corrisp a un certo vb infin
+from mlconjug3 import Conjugator
+from time import sleep
+
+conjugator = Conjugator(language="it") # così usa l'italiano
+
+def check_conjugation(verb, conjugation):
+    """
+    Verifie si une forme est bien une flexion d'un verbe.
+
+    Args:
+        verb (str): The verb to check.
+        conjugation (str): Forme a verifier.
+
+    Returns:
+        bool: Vrai si la forme est bien une flexion du verbe.
+
+    """
+    print("prova")
+    try:
+        verb_conjs = conjugator.conjugate(verb).iterate()
+        print(verb_conjs)
+
+    except:
+        for k in range(10000):
+            l = 0
+
+        print(f'except : {verb}, {conjugation}')
+
+        return False
+
+
+    return (('Indicativo', 'Indicativo presente', '3s', conjugation) in verb_conjs)
+
+
+
+def get_conj(verb):
+    """
+    Retourne la conjugaison du verbe.
+
+    Args:
+        verb (str): Le verbe.
+
+    Returns:
+        str: La conjugaison du verbe.
+
+    """
+    verb_conjs = conjugator.conjugate(verb).iterate()
+
+    for verb_conj in verb_conjs:
+        if verb_conj[0] == "Indicativo" and verb_conj[1] == "Indicativo presente" and verb_conj[2] == "3s":
+            return verb_conj[3]
+
+    return None
