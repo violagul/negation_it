3f9a21019bafc48ff991be14045288b201a90ae3
violagul <viola.gullace@sns.it>
2023-11-20 12:14:08 +0100

    Update Classifier_neg_cls_compl.py

--- 2nd_exp/Classifier_neg_cls_compl.py
+++ 2nd_exp/Classifier_neg_cls_compl.py
@@ -364,6 +364,7 @@ for gender in ["f", "m"]:
                     detail_verbs[found_verb] += 1
 
 print(f"{len(list_good_patterns_model)} matcher for the model")
+print(type(list_good_patterns_model))
 print(f"Splitting template sentences in neg and pos...")
 # create the CpTp set
 template_sentences_pos =[]
