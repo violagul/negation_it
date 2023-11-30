601da439c21bfd72d1c72df927d96ca3378c8a45
violagul <viola.gullace@sns.it>
2023-11-30 18:02:31 +0100

    Update prova2.py

--- 2nd_exp/prova2.py
+++ 2nd_exp/prova2.py
@@ -25,6 +25,7 @@ device = torch.device("cuda") if torch.cuda.is_available() else torch.devide("cp
 
 
 
+
 ########################
 ### useful functions ###
 ########################
@@ -103,249 +104,194 @@ def encode_batch(current_batch, tokenizer, model, device):
 
 
 
-size_test = 200
+size_test = 10000
 
 print(f"Downloading models...")
 # select the italian model to test
-tokenizer = AutoTokenizer.from_pretrained('dbmdz/bert-base-italian-cased')
-model_mask = AutoModelForMaskedLM.from_pretrained('dbmdz/bert-base-italian-cased').to(device)
 model = AutoModel.from_pretrained('dbmdz/bert-base-italian-cased').to(device)
-
-
-
-###########################
-### masked template set ###
-###########################
+tokenizer = AutoTokenizer.from_pretrained('dbmdz/bert-base-italian-cased')
 
 
 
 
 
 
-print(f"Building template sentences...")
-# load names, professions and verbs for the templates
-path = r"../Inputs"
-fName_file_path = f"{path}/100_names_f.txt"
-mName_file_path = f"{path}/100_names_m.txt"
-fProf_file_path = f"{path}/100_mestieri_f.txt"
-mProf_file_path = f"{path}/100_mestieri_m.txt"
-hypo_file_path = f"{path}/frasi_it.txt"
 
-fName_file = open(fName_file_path, "r")
-mName_file = open(mName_file_path, "r")
-fProf_file = open(fProf_file_path, "r")
-mProf_file = open(mProf_file_path, "r")
-
-list_verbs = load(f"{path}/base_verbs.joblib")
+##############################
+### paisa "non" extraction ###
+##############################
 
 
 
 
 
-# dictionaries of names, professions and pronouns indexed by gender for template construction
-#professionsarray = {"f": build_array(fProf_file)[:10], "m": build_array(mProf_file)[10]} 
-# buildarray is a function for creating lists from txt files        
-fprofarray = build_array(fProf_file)[10]
-mprofarray = build_array(mProf_file)[10]
-professionsarray = {"f": fprofarray, "m": mprofarray}
-fnamearray = build_array(fName_file)[20]
-mnamearray = build_array(mName_file)[20]
-name_arrays = {"f": fnamearray, "m": mnamearray}
-pronouns_maj = {"f": "Lei", "m": "Lui"}
+print(f"Uploading PAISA corpus...")
+# upload the Italian corpus
+with open(r"../data/paisa.raw.utf8", encoding='utf8') as infile:
+    paisa = infile.read()
+    paisa = paisa[:(len(paisa)//10)]
+
+print(f"Extracting Wikipedia texts from PAISA...")
+# from the corpus, select all texts containing "wiki" in their tag's url
+wiki_pattern = r"<text.*wiki.*(?:\n.*)+?\n</text>\n" 
+paisa_wiki = re.findall(wiki_pattern, paisa)
+dump(paisa_wiki, "../Inputs/paisa_wiki.joblib")
+#print(f"Number of texts from a site containing 'wiki' in their URL: {len(paisa_wiki)}")
+#paisa_wiki = paisa
+
+print(f"Extracting sentences from paisa_wiki...")
+# pattern for finding whole sentences in the texts (defined by the capital letter in the beginning, the period at the end and a minimum length)
+sent = []
+pattern = r" [A-Z][a-z ]*[,:]?[a-z ]+[,:]?[a-z ][,:]?[a-z]+\. \b"  # finds kind of acceptable sentences
+
+for text in paisa_wiki:
+  found = re.findall(pattern, text)
+  for elem in  found:
+    if len(elem) > 25:
+      sent.append(elem)
+  if len(sent)> size_test*100:
+    break
+
+#print(f"Number of sentences: {len(sent)}")
+
+
+print(f"Extracting negative sentences from paisa_wiki...")
+# splitting the sentences above into two lists:
+sent_pos = []
+sent_neg = []
+
+# pattern to find the negation in a sentence
+neg_patt = r"\b[Nn]on\b"  
+
+for s in sent:
+  matches = re.search(neg_patt, s)
+  if matches:
+    sent_neg.append(s)
+  else:
+    sent_pos.append(s)
+  if len(sent_neg) > 30 and len(sent_pos) > 30:
+     break
 
 
 
 
-
-# set up list for patterns that, for the CpTp setting, predict for the mask the same verb that was in the context
-list_good_patterns_model = []
-
-total_sentences = 0 # counts tried sentences
-tot_good_preds = 0 # counts sentences with repetition
-detail_verbs = {v : 0 for v in list_verbs} # counts, for each verb, how many times it is repeated in the mask if present in context
+size_test = min(size_test, len(sent_neg), len(sent_pos))
 
 
-size_batches = 8
-
-for gender in ["f", "m"]:
-    current_pronouns_maj = pronouns_maj[gender]
-
-    for name_available in name_arrays[gender]:
-        batch_sentences = [] # batch of sentences to try in this cycle
-        batch_verbs = [] # batch of verbs to try in this cycle
-        
-        for profession_available in professionsarray[gender]:
-            
-            current_list_verbs = list_verbs.copy()
-            shuffle(current_list_verbs)
-
-            found = False # to stop when a good verb is found
-
-            for verb_available in current_list_verbs:
-                #print(f"current verb : {verb_available}")
-                #if not complete_check and found:
-                #    break
-
-                
-                current_sentence = build_masked_context(name_available, profession_available, verb_available, current_pronouns_maj, mask_token = tokenizer.mask_token)
-
-                #print(current_sentence)
-                #quit()
-
-                batch_sentences.append(current_sentence)
-                batch_verbs.append(verb_available)
-                total_sentences += 1
-
-                
-                if total_sentences % 5000 == 0:
-                    print(f"current : {total_sentences}, found : {len(list_good_patterns_model)}")
-
-                # get the result at the end of the batch
-                if len(batch_sentences) == size_batches:
-                    new_sentence, found, nb_good_pred, found_verbs = make_and_encode_batch(batch_sentences, tokenizer, model_mask, device, batch_verbs, name_available, profession_available, current_pronouns_maj, found) 
-                    tot_good_preds+=nb_good_pred
-                    if new_sentence!= None:
-                        list_good_patterns_model.append(new_sentence)
-                    batch_sentences = []
-                    batch_verbs = []
-                    for found_verb in found_verbs:
-                        detail_verbs[found_verb] +=  1 # add one repetition to the count for the found verb
-
-
-            # repetition for what is left out of the last batch
-            if len(batch_sentences) > 0: 
-                new_sentence, found, nb_good_pred, found_verbs = make_and_encode_batch(batch_sentences, tokenizer, model_mask, device, batch_verbs, name_available, profession_available, current_pronouns_maj, found)
-
-                tot_good_preds += nb_good_pred
-                if new_sentence != None:
-                    list_good_patterns_model.append(new_sentence)
-                batch_sentences = []
-                batch_verbs = []
-                for found_verb in found_verbs:
-                    detail_verbs[found_verb] += 1
-
-
-print(f"Splitting template sentences in neg and pos...")
-# create the CpTp set
-template_sentences_pos =[]
-for pattern in list_good_patterns_model:
-  # build sentences putting the conjugated verb instead of the mask
-  sent = build_masked_context(pattern["name_available"], pattern["profession_available"],
-                               pattern["verb"], pattern["current_pronouns_maj"], pattern["masked_prediction"])
-  template_sentences_pos.append(sent)
-
-# create the CnTn set
-template_sentences_neg = []
-pat_and_repl = [[r"che ha","che non ha"],[r" Lei ", " Lei non "],[r" Lui "," Lui non "]]
-
-for sent in template_sentences_pos:
-  sent_neg = sent
-  for pair in pat_and_repl:
-    sent_neg = re.sub(pair[0], pair[1], sent_neg)
-  template_sentences_neg.append(sent_neg)
-  
-
-#############################
-### template set encoding ###
-#############################
+shuffle(sent_neg)
+shuffle(sent_pos)
 
 
 
-print(f"Extracting CLS encoding for template sentences...")
-# extract CLS for each template sentence
+# select a fixed numb of sentences to test
+sent_neg = sent_neg[:size_test]
+sent_pos = sent_pos[:size_test]
+
+print(f"Extracting CLS encodings for PAISA sentences...")
+### extract CLS
 # for each set of sentences, we encode each sentence
-print(f"Extracting CLS encoding for template sentences...")
-# extract CLS for each template sentence
-# for each set of sentences, we encode each sentence
-for sent_list in [template_sentences_neg, template_sentences_pos]:
-  batch_encoded = tokenizer.batch_encode_plus(sent_list, padding=True, add_special_tokens=True, return_tensors="pt").to(device)
 
-  # then extract only the outputs for each sentence
-  with torch.no_grad():
-    tokens_outputs = model(**batch_encoded )
+cls_encodings_neg = []
+cls_encodings_pos = []
+all_cls_encodings = []
+m = 0
+for sent_list in [sent_neg, sent_pos]:
+  for sentence in sent_list:
+    sentence_encoded = tokenizer.encode_plus(sentence, padding=True, add_special_tokens=True, return_tensors="pt").to(device)
+    if m == 0:
+      print(sentence)
 
-  # for each set of outputs we only keep the one of the CLS token, namely the first token of each sentence
-  embeddings = tokens_outputs[0]
-  cls_encodings = embeddings[:,0,:]
-
-  cls_encodings = cls_encodings.cpu().numpy()
-
-  if sent_list == template_sentences_neg:
-    cls_temp_neg = cls_encodings
-  elif sent_list == template_sentences_pos:
-    cls_temp_pos = cls_encodings
+    # then extract only the outputs for each sentence
+    with torch.no_grad():
+      tokens_outputs = model(**sentence_encoded)
 
 
-np.random.shuffle(cls_temp_neg)
-np.random.shuffle(cls_temp_pos)
-print(str(cls_temp_neg)[:200])
-cls_temp_pos = cls_temp_pos[:size_test]
-cls_temp_neg = cls_temp_neg[:size_test]
+    # for each set of outputs we only keep the one of the CLS token, namely the first token of each sentence
+    embeddings = tokens_outputs[0]
+    cls_encodings = embeddings[:, 0, :]
 
+    m+=1
+    cls_encodings = cls_encodings.cpu().numpy()
+    if m == 1:
+        all_cls_encodings = cls_encodings
+        print(cls_encodings.shape)
+    if m > 1:
+        all_cls_encodings = np.vstack((all_cls_encodings,cls_encodings))
+   
 
-
-
-
-
-
-############################
-### masked template test ###
-############################
+  if sent_list == sent_neg:
+    cls_encodings_neg = cls_encodings
+  elif sent_list == sent_pos:
+    cls_encodings_pos = cls_encodings
 
 
 
+#train = torch.zeros(cls_encodings_neg.shape[0]*2, cls_encodings_neg.shape[1])
+#train[cls_encodings_neg.shape[0]] = cls_encodings_neg[:9000]
+#train = train.append(cls_encodings_pos[:9000])
 
-#train_temp = np.concatenate((cls_encodings_pos[:train_size], cls_encodings_neg[:train_size]))
-#train_temp_lab = np.concatenate((np.zeros(train_size), np.ones(train_size)))
-#test_temp = np.concatenate((cls_encodings_pos[train_size:], cls_encodings_neg[train_size:]))
-#test_temp_lab = np.concatenate((np.zeros(test_size), np.ones(test_size)))
-
-
-
-test_temp = np.concatenate((cls_temp_pos[:size_test], cls_temp_neg[:size_test]))
-print(str(test_temp)[:2000])
-print(test_temp.shape)
-print(cls_temp_neg.shape)
-test_temp_lab = np.concatenate((np.zeros(size_test), np.ones(size_test)))
+# we use 90% of data as training and 10% as test
+train_size = round(size_test*0.9)
+train = np.concatenate((cls_encodings_pos[:train_size], cls_encodings_neg[:train_size]), 0) # shape num_sent x 768
+labels = np.concatenate((np.zeros(train_size), np.ones(train_size)))
+test = np.concatenate((cls_encodings_pos[train_size:], cls_encodings_neg[train_size:]), 0)
+test_size = int(size_test - train_size)
+test_lab = np.concatenate((np.zeros(test_size), np.ones(test_size)))
 
 
-
-#scaler.fit(train_temp)
-#train = scaler.transform(train_temp)
-#test_2 = scaler.transform(test_temp)
-
-
-#data normalization
+# data normalization
 scaler = StandardScaler()
-scaler.fit(test_temp)
-test_2 = scaler.transform(test_temp)
+scaler.fit(train)
+dati_scaled = scaler.transform(train)
+dump(scaler, f"../Inputs/scaler.joblib")
+
+
+X = dati_scaled 
+test = scaler.transform(test)
+#print(test)
+#print(test_lab)
+
+y = labels
+    
+X, y = skshuffle(X, y, random_state=42)
 
 
 
 
 
-#########################################
-### classification with the MLP model ###
-#########################################
+########################################
+### classifier creation and training ###
+########################################
 
 
-print("Testing with MLP classifiers...")
+paisa_result =[]
 
-template_result = []
+print(f"Training and testing MLP...")
+# set up the MLP classifier
+# solver : adam or sgd
+# hidden_layer_sizes : 40,40 or 350,350
+# alpha : between 1e-5 and 1e-3
+n=0
+for hl in [(350,350),(40,40)]:
+  for a in [1e-5, 1e-4, 1e-3]:
+    for solv in ["adam", "sgd"]:
+      n+=1
+      clf = MLPClassifier(solver = solv, alpha = a,
+                    hidden_layer_sizes=hl, random_state = 1)
 
-for n in range(1, 13):
-   clf = load(f"../Inputs/non_classifier_{n}.joblib")
-   
-   
-   predicted = clf.predict(test_2)
-   right_pred = clf.score(test_2, test_temp_lab)
-   tn, fp, fn, tp = confusion_matrix(test_temp_lab, predicted).ravel()
-   template_result.append(f"Method: {solv}\tNb hidden layers: {str(hl)}\tAlpha: {str(a)}\n {right_pred}%\n\nTrue neg = {tn}\nFalse pos = {fp}\nFalse neg = {fn}\nTrue pos = {tp}")
+      # train on data
+      clf = clf.fit(X, y)
 
+      # see predictions on the dataset
+      predicted = clf.predict(test)
+      right_pred = clf.score(test, test_lab)
+      tn, fp, fn, tp = confusion_matrix(test_lab, predicted).ravel()
+      paisa_result.append(f"Method: {solv}\nNb hidden layers: {str(hl)}\nAlpha: {str(a)}\nScore : {right_pred}\nTrue neg = {tn}\nFalse pos = {fp}\nFalse neg = {fn}\nTrue pos = {tp}\n\n")
 
+     
+      dump(clf, f"../Inputs/non_classifier_{n}.joblib")
+    
 
-
-
-print("TEMPLATE TEST\n\n")
-for scores in template_result:
+print("PAISA' TEST\n")
+for scores in paisa_result:
    print(scores)
