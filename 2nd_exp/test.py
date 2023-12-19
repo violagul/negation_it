021dd6a6f534836f1932f5eb28920bcff4a4b3db
violagul <viola.gullace@sns.it>
2023-12-19 11:26:46 +0100

    Update test.py

--- 2nd_exp/test.py
+++ 2nd_exp/test.py
@@ -136,7 +136,7 @@ mName_file = open(mName_file_path, "r")
 fProf_file = open(fProf_file_path, "r")
 mProf_file = open(mProf_file_path, "r")
 
-list_verbs = load(f"{path}/base_verbs.joblib")[:10]
+list_verbs = load(f"{path}/base_verbs.joblib")
 
 
 
@@ -145,11 +145,11 @@ list_verbs = load(f"{path}/base_verbs.joblib")[:10]
 # dictionaries of names, professions and pronouns indexed by gender for template construction
 #professionsarray = {"f": build_array(fProf_file)[:10], "m": build_array(mProf_file)[10]} 
 # buildarray is a function for creating lists from txt files        
-fprofarray = build_array(fProf_file)[:8]
-mprofarray = build_array(mProf_file)[:8]
+fprofarray = build_array(fProf_file)
+mprofarray = build_array(mProf_file)
 professionsarray = {"f": fprofarray, "m": mprofarray}
-fnamearray = build_array(fName_file)[:8]
-mnamearray = build_array(mName_file)[:8]
+fnamearray = build_array(fName_file)
+mnamearray = build_array(mName_file)
 name_arrays = {"f": fnamearray, "m": mnamearray}
 pronouns_maj = {"f": "Lei", "m": "Lui"}
 
@@ -163,8 +163,6 @@ template_sentences_pos = []
 total_sentences = 0 # counts tried sentences
 tot_good_preds = 0 # counts sentences with repetition
 detail_verbs = {v : 0 for v in list_verbs} # counts, for each verb, how many times it is repeated in the mask if present in context
-
-
 
 
 for gender in ["f", "m"]:
@@ -190,9 +188,14 @@ for gender in ["f", "m"]:
                 #if not complete_check and found:
                 #    break
 
-                
-                current_sentence = build_masked_context(name_available, profession_available, verb_available, current_pronouns_maj, mask_token = verb_available)
-
+                template_Cp = "NOM è PROF che ha l'abitudine di ACT."
+                pattern_n = r"NOM"
+                pattern_p = r"PROF"
+                pattern_a = r"ACT"
+                #current_sentence = build_masked_context(name_available, profession_available, verb_available, current_pronouns_maj, mask_token = verb_available)
+                current_sentence = re.sub(pattern_n, name_available, template_Cp)
+                current_sentence = re.sub(pattern_p, profession_available, template_Cp)
+                current_sentence = re.sub(pattern_a, verb_available, template_Cp)
                 #print(current_sentence)
                 #quit()
 
@@ -201,12 +204,46 @@ for gender in ["f", "m"]:
 
                 
                 #if total_sentences % 5000 == 0:
-                if total_sentences % 100 == 0:
+                if total_sentences % 400 == 0:
                     print(f"current : {total_sentences}")
+                    print(current_sentence)
                 
                 if total_sentences > 300:
                     break
-'''
+
+'''for gender in ["f", "m"]:
+    current_pronouns_maj = pronouns_maj[gender]
+
+    for name_available in name_arrays[gender]:
+        batch_sentences = [] # batch of sentences to try in this cycle
+        batch_verbs = [] # batch of verbs to try in this cycle
+        
+        for profession_available in professionsarray[gender]:
+            
+            current_list_verbs = list_verbs.copy()
+            shuffle(current_list_verbs)
+
+            found = False # to stop when a good verb is found
+
+            for verb_available in current_list_verbs:
+                #print(f"current verb : {verb_available}")
+                #if not complete_check and found:
+                #    break
+
+                
+                current_sentence = build_masked_context(name_available, profession_available, verb_available, current_pronouns_maj, mask_token = tokenizer.mask_token)
+
+                #print(current_sentence)
+                #quit()
+
+                batch_sentences.append(current_sentence)
+                batch_verbs.append(verb_available)
+                total_sentences += 1
+
+                
+                if total_sentences % 5000 == 0:
+                    print(f"current : {total_sentences}, found : {len(list_good_patterns_model)}")
+
                 # get the result at the end of the batch
                 if len(batch_sentences) == size_batches:
                     new_sentence, found, nb_good_pred, found_verbs = make_and_encode_batch(batch_sentences, tokenizer, model_mask, device, batch_verbs, name_available, profession_available, current_pronouns_maj, found) 
@@ -235,29 +272,27 @@ for gender in ["f", "m"]:
 print(f"Splitting template sentences in neg and pos...")
 # create the CpTp set
 
-
+'''for pattern in template_sentences_pos:
+  # build sentences putting the conjugated verb instead of the mask
+  sent = build_masked_context(pattern["name_available"], pattern["profession_available"],
+                               pattern["verb"], pattern["current_pronouns_maj"], pattern["masked_prediction"])
+  template_sentences_pos.append(sent)
+'''
 # create the CnTn set
-template_sentences_CnTn = []
-template_sentences_CnTp = []
-template_sentences_CpTn = []
+template_sentences_Cn = []
 
-pat_and_repl = [[r"che ha","che non ha"],[r" Lei ", " Lei non "],[r" Lui "," Lui non "]]
+pat_and_repl = [r"che ha","che non ha"]
 
 for sent in template_sentences_pos:
     sent_neg = sent
-    sent_CnTp = re.sub(pat_and_repl[0][0], pat_and_repl[0][1], sent_neg)
-    sent_CpTn = re.sub(pat_and_repl[1][0], pat_and_repl[1][1], sent_neg)
-    sent_CpTn = re.sub(pat_and_repl[2][0], pat_and_repl[2][1], sent_CpTn)
-    sent_CnTn = re.sub(pat_and_repl[0][0], pat_and_repl[0][1], sent_CpTn)
+    sent_Cn = re.sub(pat_and_repl[0], pat_and_repl[1], sent_neg)
 
-    template_sentences_CnTn.append(sent_CnTn)
-    template_sentences_CnTp.append(sent_CnTp)
-    template_sentences_CpTn.append(sent_CpTn)
+    template_sentences_Cn.append(sent_Cn)
 
 
 
 
-
+print(f"\nCp : {len(template_sentences_pos)}\nCn : {len(template_sentences_Cn)}\n")
 
 
 #############################
@@ -271,7 +306,7 @@ print(f"Extracting CLS encoding for template sentences...")
 # for each set of sentences, we encode each sentence
 
 all_cls_encodings = []
-for templ_list in [template_sentences_CnTn, template_sentences_CnTp, template_sentences_CpTn, template_sentences_pos]:
+for templ_list in [template_sentences_Cn, template_sentences_pos]:
   m = 0 
   for sentence in templ_list:
     
@@ -293,33 +328,24 @@ for templ_list in [template_sentences_CnTn, template_sentences_CnTp, template_se
         all_cls_encodings = cls_encodings
     if m > 1:
         all_cls_encodings = np.vstack((all_cls_encodings,cls_encodings))
+    if m % 50 == 0:
+        print(str(m) + "\textracted")
    
     
    
    
-  if templ_list == template_sentences_CnTn:
-      cls_temp_CnTn = all_cls_encodings
-  elif templ_list == template_sentences_CnTp:
-      cls_temp_CnTp = all_cls_encodings
-  elif templ_list == template_sentences_CpTn:
-      cls_temp_CpTn = all_cls_encodings
+  if templ_list == template_sentences_Cn:
+      cls_temp_Cn = all_cls_encodings
   elif templ_list == template_sentences_pos:
       cls_temp_pos = all_cls_encodings
      
 
 
-np.random.shuffle(cls_temp_CnTn)
-np.random.shuffle(cls_temp_CnTp)
-np.random.shuffle(cls_temp_CpTn)
+np.random.shuffle(cls_temp_Cn)
 np.random.shuffle(cls_temp_pos)
 
-
-size_test = min(size_test, len(cls_temp_CnTn), len(cls_temp_CnTp), len(cls_temp_CpTn), len(cls_temp_pos))
 cls_temp_pos = cls_temp_pos[:size_test]
-cls_temp_CnTn = cls_temp_CnTn[:size_test]
-cls_temp_CnTp = cls_temp_CnTp[:size_test]
-cls_temp_CpTn = cls_temp_CpTn[:size_test]
-
+cls_temp_Cn = cls_temp_Cn[:size_test]
 
 
 
@@ -341,16 +367,10 @@ cls_temp_CpTn = cls_temp_CpTn[:size_test]
 
 
 test_temp_pos = cls_temp_pos[:size_test]
-test_temp_CnTn = cls_temp_CnTn[:size_test]
-test_temp_CnTp = cls_temp_CnTp[:size_test]
-test_temp_CpTn = cls_temp_CpTn[:size_test]
-
+test_temp_Cn = cls_temp_Cn[:size_test]
 
 test_temp_lab_pos = np.zeros(size_test)
-test_temp_lab_CnTn = np.ones(size_test)
-test_temp_lab_CnTp = np.ones(size_test)
-test_temp_lab_CpTn = np.ones(size_test)
-
+test_temp_lab_Cn = np.ones(size_test)
 
 
 
@@ -358,9 +378,7 @@ test_temp_lab_CpTn = np.ones(size_test)
 #data normalization
 scaler = load(f"../Inputs/scaler.joblib")
 test_2_pos = scaler.transform(test_temp_pos)
-test_2_CnTn = scaler.transform(test_temp_CnTn)
-test_2_CnTp = scaler.transform(test_temp_CnTp)
-test_2_CpTn = scaler.transform(test_temp_CpTn)
+test_2_Cn = scaler.transform(test_temp_Cn)
 
 
 
@@ -374,10 +392,11 @@ test_2_CpTn = scaler.transform(test_temp_CpTn)
 print("Testing with MLP classifiers...")
 
 template_result_pos = []
-template_result_CnTn = []
-template_result_CnTp = []
-template_result_CpTn = []
+template_result_Cn = []
 
+for n in range(1, 13):
+   clf = load(f"../Inputs/non_classifier_{n}.joblib")
+   
 for n in range(1, 13):
    clf = load(f"../Inputs/non_classifier_{n}.joblib")
    
@@ -394,38 +413,41 @@ for n in range(1, 13):
 
 
 
+   predicted = clf.predict(test_2_Cn)
+   right_pred = clf.score(test_2_Cn, test_temp_lab_Cn)
+   if len(confusion_matrix(test_temp_lab_Cn, predicted).ravel()) == 4:
+       tn, fp, fn, tp = confusion_matrix(test_temp_lab_Cn, predicted).ravel()
+       template_result_Cn.append(f"Score\t{right_pred}\n\nTrue neg\t{tn}\nFalse pos\t{fp}\nFalse neg\t{fn}\nTrue pos\t{tp}\n\n")
+   else:
+       conf_matr = confusion_matrix(test_temp_lab_Cn, predicted).ravel()
+       template_result_Cn.append(f"Score\t{right_pred}\n\nConfusion matrix : {conf_matr}\n\n")
+
+'''   
+   predicted = clf.predict(test_2_pos)
+   right_pred = clf.score(test_2_pos, test_temp_lab_pos)
+   tn, fp, fn, tp = confusion_matrix(test_temp_lab_pos, predicted).ravel()
+   #template_result.append(f"Method\t{solv}\nNb hidden layers\t{str(hl)}\nAlpha\t{str(a)}\nScores\t{right_pred}\n\nTrue neg\t{tn}\nFalse pos\t{fp}\nFalse neg\t{fn}\nTrue pos\t{tp}\n\n")
+   template_result_pos.append(f"Score\t{right_pred}\n\nTrue neg\t{tn}\nFalse pos\t{fp}\nFalse neg\t{fn}\nTrue pos\t{tp}\n\n")
+
    predicted = clf.predict(test_2_CnTn)
    right_pred = clf.score(test_2_CnTn, test_temp_lab_CnTn)
-   if len(confusion_matrix(test_temp_lab_CnTn, predicted).ravel()) == 4:
-       tn, fp, fn, tp = confusion_matrix(test_temp_lab_CnTn, predicted).ravel()
-       template_result_CnTn.append(f"Score\t{right_pred}\n\nTrue neg\t{tn}\nFalse pos\t{fp}\nFalse neg\t{fn}\nTrue pos\t{tp}\n\n")
-   else:
-       conf_matr = confusion_matrix(test_temp_lab_CnTn, predicted).ravel()
-       template_result_CnTn.append(f"Score\t{right_pred}\n\nConfusion matrix : {conf_matr}\n\n")
-
-
+   tn, fp, fn, tp = confusion_matrix(test_temp_lab_CnTn, predicted).ravel()
+   #template_result.append(f"Method\t{solv}\nNb hidden layers\t{str(hl)}\nAlpha\t{str(a)}\nScores\t{right_pred}\n\nTrue neg\t{tn}\nFalse pos\t{fp}\nFalse neg\t{fn}\nTrue pos\t{tp}\n\n")
+   template_result_CnTn.append(f"Score\t{right_pred}\n\nTrue neg\t{tn}\nFalse pos\t{fp}\nFalse neg\t{fn}\nTrue pos\t{tp}\n\n")
 
    predicted = clf.predict(test_2_CnTp)
    right_pred = clf.score(test_2_CnTp, test_temp_lab_CnTp)
-   if len(confusion_matrix(test_temp_lab_CnTp, predicted).ravel()) == 4:
-       tn, fp, fn, tp = confusion_matrix(test_temp_lab_CnTp, predicted).ravel()
-       template_result_CnTp.append(f"Score\t{right_pred}\n\nTrue neg\t{tn}\nFalse pos\t{fp}\nFalse neg\t{fn}\nTrue pos\t{tp}\n\n")
-   else:
-       conf_matr = confusion_matrix(test_temp_lab_CnTp, predicted).ravel()
-       template_result_CnTp.append(f"Score\t{right_pred}\n\nConfusion matrix : {conf_matr}\n\n")
-   
-
+   tn, fp, fn, tp = confusion_matrix(test_temp_lab_CnTp, predicted).ravel()
+   #template_result.append(f"Method\t{solv}\nNb hidden layers\t{str(hl)}\nAlpha\t{str(a)}\nScores\t{right_pred}\n\nTrue neg\t{tn}\nFalse pos\t{fp}\nFalse neg\t{fn}\nTrue pos\t{tp}\n\n")
+   template_result_CnTp.append(f"Score\t{right_pred}\n\nTrue neg\t{tn}\nFalse pos\t{fp}\nFalse neg\t{fn}\nTrue pos\t{tp}\n\n")
 
    predicted = clf.predict(test_2_CpTn)
    right_pred = clf.score(test_2_CpTn, test_temp_lab_CpTn)
-   if len(confusion_matrix(test_temp_lab_CpTn, predicted).ravel()) == 4:
-       tn, fp, fn, tp = confusion_matrix(test_temp_lab_CpTn, predicted).ravel()
-       template_result_CpTn.append(f"Score\t{right_pred}\n\nTrue neg\t{tn}\nFalse pos\t{fp}\nFalse neg\t{fn}\nTrue pos\t{tp}\n\n")
-   else:
-       conf_matr = confusion_matrix(test_temp_lab_CpTn, predicted).ravel()
-       template_result_CpTn.append(f"Score\t{right_pred}\n\nConfusion matrix : {conf_matr}\n\n")
+   tn, fp, fn, tp = confusion_matrix(test_temp_lab_CpTn, predicted).ravel()
+   #template_result.append(f"Method\t{solv}\nNb hidden layers\t{str(hl)}\nAlpha\t{str(a)}\nScores\t{right_pred}\n\nTrue neg\t{tn}\nFalse pos\t{fp}\nFalse neg\t{fn}\nTrue pos\t{tp}\n\n")
+   template_result_CpTn.append(f"Score\t{right_pred}\n\nTrue neg\t{tn}\nFalse pos\t{fp}\nFalse neg\t{fn}\nTrue pos\t{tp}\n\n")
 
-
+'''
 
 
 
@@ -434,16 +456,6 @@ for scores in template_result_pos:
    print(scores)
 
 
-print("TEMPLATE TEST CnTn\n\n")
-for scores in template_result_CnTn:
-   print(scores)
-
-
-print("TEMPLATE TEST CnTp\n\n")
-for scores in template_result_CnTp:
-   print(scores)
-
-
-print("TEMPLATE TEST CpTn\n\n")
-for scores in template_result_CpTn:
+print("TEMPLATE TEST Cn\n\n")
+for scores in template_result_Cn:
    print(scores)
