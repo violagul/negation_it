133a8d5ba854753a17f1c774534cffd8073cbcdc
violagul <viola.gullace@sns.it>
2024-01-22 14:40:11 +0100

    Update paisa_sent_extractor.py

--- 2nd_exp/paisa_sent_extractor.py
+++ 2nd_exp/paisa_sent_extractor.py
@@ -165,6 +165,7 @@ for text in paisa_wiki:
          if verb in elem:
             sent.append(elem[:1])
             if len(sent)%1000 ==0:
+               print(elem[:1])
                print(f"{len(sent)} frasi trovate")
   if len(sent)> size_test*100:
     break
