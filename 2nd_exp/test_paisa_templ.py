3c8c6cdf11d48d009fa2f2cf4cb5faa76b2caf85
violagul <viola.gullace@sns.it>
2024-01-26 13:47:16 +0100

    Update test_paisa_templ.py

--- 2nd_exp/test_paisa_templ.py
+++ 2nd_exp/test_paisa_templ.py
@@ -369,7 +369,7 @@ for n in range(1, 13):
    
     for templ_sent, templ_list in paisa_neg_templ_test.items():
         paisa_neg_res.append(n)
-        paisa_neg_res.append(templ_sent, "\n")
+        paisa_neg_res.append(templ_sent)
         predicted = clf.predict(templ_list)
         right_pred = clf.score(templ_list, neg_paisa_lab[templ_sent])
         if len(confusion_matrix(neg_paisa_lab[templ_sent], predicted).ravel()) == 4:
@@ -384,7 +384,7 @@ for n in range(1, 13):
    
     for templ_sent, templ_list in paisa_pos_templ_test.items():
         paisa_pos_res.append(n)
-        paisa_pos_res.append(templ_sent,"\n")
+        paisa_pos_res.append(templ_sent)
         predicted = clf.predict(templ_list)
         right_pred = clf.score(templ_list, pos_paisa_lab[templ_sent])
         if len(confusion_matrix(pos_paisa_lab[templ_sent], predicted).ravel()) == 4:
