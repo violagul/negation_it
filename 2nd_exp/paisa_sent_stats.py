c254b04830a238b818e00a25071f88e44c3d017c
violagul <viola.gullace@sns.it>
2024-01-29 14:54:36 +0100

    Update paisa_sent_stats.py

--- 2nd_exp/paisa_sent_stats.py
+++ 2nd_exp/paisa_sent_stats.py
@@ -167,6 +167,6 @@ for elem in range(5,57):
 '''
 
 pyplot.bar(list(pos_tok_size_dict.keys()), list(pos_tok_size_dict.values()))
-pyplot.savefig("plot_pos.png")'''
-pyplot.bar(list(neg_tok_size_dict.keys()), list(neg_tok_size_dict.values()))
+pyplot.savefig("plot_pos.png")
+'''pyplot.bar(list(neg_tok_size_dict.keys()), list(neg_tok_size_dict.values()))
 pyplot.savefig("plot_neg.png")'''
