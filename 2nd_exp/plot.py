8d2baf676f235c3530a77ee66512b214fb9a2121
violagul <viola.gullace@sns.it>
2024-04-03 18:36:18 +0200

    plots

--- 2nd_exp/plot.py
+++ 2nd_exp/plot.py
@@ -17,10 +17,15 @@ bar4 = [i+w for i in bar3]
 bar5 = [i+w for i in bar4]
 
 bert_it = [83.55, 88.97, 88.42, 98.72]
+bert_it = [100-x for x in bert_it]
 mbert = [77.03, 80.34, 81.37, 99.03]
+mbert = [100-x for x in mbert]
 alberto = [90.33, 95.64, 90.66, 99.85]
+alberto= [100-x for x in alberto]
 umberto = [90.09, 88.13, 79.42, 98.35]
+umberto = [100-x for x in umberto]
 bertxxl = [77.87, 85.46, 85.40, 85.66]
+bertxxl = [100-x for x in bertxxl]
 
 pyplot.bar(bar1, bert_it,w ,label = "BERT italian")
 pyplot.bar(bar2, mbert, w,label = "mBERT")
@@ -28,7 +33,7 @@ pyplot.bar(bar3, alberto,w ,label = "AlBERTo")
 pyplot.bar(bar4, umberto,w ,label = "UmBERTo")
 pyplot.bar(bar5, bertxxl,w ,label = "BERT xxl")
 pyplot.xticks(bar3, contxt)
-pyplot.ylabel("Percentage of repetition")
+pyplot.ylabel("Drop of repetition")
 
 matplotlib.pyplot.title("Repetitions")
 pyplot.legend(loc='upper left', ncols=3, fontsize=7)
