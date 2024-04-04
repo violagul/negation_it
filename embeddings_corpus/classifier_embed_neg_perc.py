97e475b3ddcdd553dc0e0539cb71a8771d2afe13
violagul <viola.gullace@sns.it>
2024-04-04 12:40:59 +0200

    Update classifier_embed_neg_perc.py

--- embeddings_corpus/classifier_embed_neg_perc.py
+++ embeddings_corpus/classifier_embed_neg_perc.py
@@ -80,9 +80,9 @@ pyplot.show()'''
 print("Loading embeddings...")
 embs = torch.load(r"/data/vgullace/embeddings990000")
 # dictionary
-# keys = verb; velues = list of lists (list of negative embeddings [0], list of positive embeddings [0])
+# keys = verb; values = list of lists (list of negative embeddings [0], list of positive embeddings [0])
 
-
+{v1 : [[], []], v2 : [[],[]]}
 
 print("Training on verbs with > 0.1 neg perc")
 vbs_oneperc = [v for v in vb_perc_mx.keys() if vb_perc_mx[v]>=0.1]
@@ -104,7 +104,7 @@ train_size = round(len(neg_embs)*0.9)
 
 train_data = np.concatenate((neg_embs[:train_size], pos_embs[:train_size]), 0)
 
-train_labs = np.concatenate(np.zeros(train_size), np.ones(train_size), 0)
+train_labs = np.concatenate((np.zeros(train_size), np.ones(train_size)), 0)
 print(train_data.shape)
 print(train_labs.shape)
 
